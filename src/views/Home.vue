732b6dc8daefdcb2a375f96458307bd5aae2d4aa
Polo <paulnaszalyi@yahoo.fr>
2019-02-01 13:28:50 +0100

    Paul

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="home">
-    <p class="h2">BLAST PRO SERIES</p>
-    <div class="video-youtube">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/E4pyzeTvFC0" frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-      </iframe>
+    <div class="blast-pro-series">
+      <p class="h2">BLAST PRO SERIES</p>
+      <div class="video-youtube">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/E4pyzeTvFC0" frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      </div>
     </div>
 
-    <p class="h2">SHOP</p>
-    <div class="shop"></div>
-    <div class="shop"></div>
+    <div class="shop">
+      <p class="h2">SHOP</p>
+      <div class="centre">
+        <div class="container-shop">
+          <p>Test</p>
+        </div>
+        <div class="container-shop">
+          <p>Test</p>
+        </div>
+      </div>
+    </div>
 
-    <p class="h2">BALANCE</p>
+    <div class="balance">
+      <p class="h2">BALANCE</p>
+    </div>
   </div>
 </template>
